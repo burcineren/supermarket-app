04efef1f9c2670004444d7c0d3a41a721cbb8d18
burcineren <burcin.ern969@gmail.com>
2024-11-30 15:20:10 +0300

    chore: route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {MarketComponent} from "./pages/market/market.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./pages/market/market.component').then(m => m.MarketComponent)
+  },
+];
