ed1a8f827621d1a82b86c5adb7f69f28192ad05f
burcineren <burcin.ern969@gmail.com>
2024-12-01 12:46:52 +0300

    feat: navbar

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,17 +1,36 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ExchangeRateService } from '../../core/services/exchange-rate.service';
+import { FormsModule } from '@angular/forms';
 
+export interface Product {
+  id: number;
+  name: string;
+}
+
+export interface Section {
+  id: number;
+  name: string;
+  products: Product[];
+}
+
+export interface Market {
+  id: number;
+  name: string;
+  sections: Section[];
+}
 @Component({
   selector: 'navbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
     exchangeRates: { currency: string; value: number }[] = []; // Döviz kurları için bir dizi
-
+    searchTerm: string = '';
+    markets: Market[] = [];
+    filteredMarkets: Market[] = [];
   constructor(private exchangeRateService: ExchangeRateService) {}
 
   ngOnInit() {
@@ -23,5 +42,22 @@ export class NavbarComponent {
         })
       );
     });
+  }
+  filterMarkets() {
+    const term = this.searchTerm.toLowerCase();
+    this.filteredMarkets = this.markets
+      .map((market) => {
+        const filteredSections = market.sections
+          .map((section) => {
+            const filteredProducts = section.products.filter((product) =>
+              product.name.toLowerCase().includes(term)
+            );
+            return { ...section, products: filteredProducts };
+          })
+          .filter((section) => section.products.length > 0);
+
+        return { ...market, sections: filteredSections };
+      })
+      .filter((market) => market.sections.length > 0);
   }
 }
